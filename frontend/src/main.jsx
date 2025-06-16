890eb401c4fb7c326009a99a5146fe2edf147113
GaryOcean <81794144+GaryOcean428@users.noreply.github.com>
2025-06-16 16:14:11 +0800

    fix: correct strict mode environment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "@/App.jsx";
 import "@/index.css";
 const isDev = process.env.NODE_ENV !== "production";
-const REACTWRAP = isDev ? React.Fragment : React.StrictMode;
+const REACTWRAP = isDev ? React.StrictMode : React.Fragment;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <REACTWRAP>
